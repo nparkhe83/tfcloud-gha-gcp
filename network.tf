60380ed49a9b461850376391f3324f7c612e3001
Nilesh <parkhenilesh@outlook.com>
2024-02-29 12:18:54 +0530

    wip

--- network.tf
+++ network.tf
@@ -10,3 +10,15 @@ resource "google_compute_firewall" "allow_web" {
     ports    = ["8080"]
   }
 }
+
+resource "google_compute_network" "vpc_network" {
+  name                    = default
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "jsd_subnetwork" {
+  name          = default_subnet
+  ip_cidr_range = "10.0.20.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
+}
