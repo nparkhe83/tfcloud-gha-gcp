d20995e11628b4b6ae82f98aae7793ad390d545d
Nilesh <parkhenilesh@outlook.com>
2024-02-29 14:16:07 +0530

    wip

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,3 @@
-# output "web-address" {
-#   value = "${google_compute_instance.web.network_interface[0].access_config[0].nat_ip}:8080"
-# }
 
 output "temp-address" {
   value = google_compute_instance.web
